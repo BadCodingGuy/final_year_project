440de8984c36b31ae62fa8a62fc8b06f5e1a06a1
megabrokeboi <daniel.c.j.wood@gmail.com>
2024-02-21 02:00:44 +0000

    Firebase set up

--- lib/Screens/main.dart
+++ lib/Screens/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb){
+    await Firebase.initializeApp(options: FirebaseOptions(apiKey: "AIzaSyDGiidEEu9r056dZJhXNeFVQJwmKv4sRqU", appId: "1:941947686765:web:966510ae75ba15c1381464", messagingSenderId: "941947686765", projectId: "final-year-project-8a666"));
+  }
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:final_year_project/Screens/Home page.dart';
+import 'package:final_year_project/Screens/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
