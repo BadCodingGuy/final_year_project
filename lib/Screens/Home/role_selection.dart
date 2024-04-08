3807feecfa08bf03ce4e9f5a799bf4cbb0aa4f07
megabrokeboi <daniel.c.j.wood@gmail.com>
2024-04-08 21:13:10 +0100

    finishing touches

--- lib/Screens/Home/role_selection.dart
+++ lib/Screens/Home/role_selection.dart
@@ -1,2 +1,42 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+
+
+class RoleSelector extends StatefulWidget {
+  @override
+  _RoleSelectorState createState() => _RoleSelectorState();
+}
+
+class _RoleSelectorState extends State<RoleSelector> {
+  String selectedUserRole = '';
+
+  String recordUserChoice(String role) {
+    setState(() {
+      selectedUserRole = role;
+    });
+    return role;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Role Selection'),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          ElevatedButton(
+            onPressed: () => recordUserChoice('Teacher'),
+            child: Text('Teacher'),
+          ),
+          ElevatedButton(
+            onPressed: () => recordUserChoice('Student'),
+            child: Text('Student'),
+          ),
+        ],
+      ),
+    );
+  }
+}
