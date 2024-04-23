0baaaf00ecd8828d81efc485036f8b7fab475b4b
megabrokeboi <daniel.c.j.wood@gmail.com>
2024-04-23 16:31:52 +0100

    revision complete

--- lib/Screens/Student pages/revison.dart
+++ lib/Screens/Student pages/revison.dart
@@ -1,23 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../Home/student_home.dart';
-import '../../main.dart';
 
-class RevisionPage extends StatelessWidget {
+class RevisionPage extends StatefulWidget {
+  @override
+  _RevisionPageState createState() => _RevisionPageState();
+}
+
+class _RevisionPageState extends State<RevisionPage> {
+  late VideoPlayerController _videoPlayerController;
+  late ChewieController _chewieController;
+  String? _selectedExamBoard;
+  String? _selectedTopic;
+
+  @override
+  void initState() {
+    super.initState();
+    _videoPlayerController = VideoPlayerController.asset('assets/example_video.mp4');
+
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController,
+      autoPlay: false,
+      looping: true,
+      aspectRatio: 16 / 9,
+      allowFullScreen: true,
+      showControlsOnInitialize: true,
+      showControls: true,
+      deviceOrientationsAfterFullScreen: [DeviceOrientation.portraitUp],
+      deviceOrientationsOnEnterFullScreen: [DeviceOrientation.landscapeRight, DeviceOrientation.landscapeLeft],
+    );
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    _chewieController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.brown[200],
       appBar: AppBar(
         backgroundColor: Colors.brown[600],
-        title: Text('Revision Page',
+        title: Text('Revise',
           style: TextStyle(
             fontFamily: 'Jersey 10', // Use your font family name here
             fontSize: 30,
           ),
         ),
-        elevation: 0.0,
         leading: GestureDetector(
           onTap: () {
             Navigator.push(
@@ -28,60 +64,184 @@ class RevisionPage extends StatelessWidget {
           child: Image.asset('assets/logo.png', height: 40, width: 40), // Replace with your logo path
         ),
       ),
-      body: Center(
-        child: GridView.count(
-          crossAxisCount: 2, // Number of columns
-          mainAxisSpacing: 20.0, // Spacing between rows
-          crossAxisSpacing: 20.0, // Spacing between columns
-          padding: EdgeInsets.all(20.0), // Padding around the grid
-          children: [
-            InkWell(
-              onTap: () {
-                _launchURL('https://student.craigndave.org/');
-              },
-              child: Image.asset(
-                'assets/Craig-n-Dave.png',
-                width: 80, // Reduced width
-                height: 80, // Reduced height
+      body: Row(
+        children: <Widget>[
+          // Left side content
+          Expanded(
+            flex: 1,
+            child: Column(
+              children: <Widget>[
+                // Video player
+                Expanded(
+                  child: Center(
+                    child: Chewie(
+                      controller: _chewieController,
+                    ),
+                  ),
+                ),
+
+                // Buttons
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        // First Column of Buttons
+                        _buildButton('assets/bitesize.png', _getLinkBitesize()),
+
+                        // Second Column of Buttons
+                        _buildButton('assets/revision.jpg', _getLinkRevision()),
+
+                        // Third Column of Buttons
+                        _buildButton('assets/ada.png', _getLinkAda()),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // Right side content
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'What exam board are you using?',
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Expanded(
+                      child: DropdownButton<String>(
+                        isExpanded: true,
+                        hint: Text('Select exam board'),
+                        value: _selectedExamBoard,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            _selectedExamBoard = newValue;
+                            _updateVideo();
+                          });
+                        },
+                        items: <String>['AQA', 'OCR', 'Eduqas', 'Edexcel'].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'What do you need to learn?',
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Expanded(
+                      child: DropdownButton<String>(
+                        isExpanded: true,
+                        hint: Text('Select topic'),
+                        value: _selectedTopic,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            _selectedTopic = newValue;
+                            _updateVideo();
+                          });
+                        },
+                        items: <String>[
+                          'Programming Basics',
+                          'Data Structures',
+                          'Algorithms',
+                          'Networking',
+                          'Databases'
+                        ].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-            InkWell(
-              onTap: () {
-                _launchURL('https://www.bbc.co.uk/bitesize/subjects/z34k7ty');
-              },
-              child: Image.asset(
-                'assets/bitesize.png',
-                width: 80, // Reduced width
-                height: 80, // Reduced height
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                _launchURL('https://adacomputerscience.org/');
-              },
-              child: Image.asset(
-                'assets/ada.png',
-                width: 80, // Reduced width
-                height: 80, // Reduced height
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                _launchURL('https://revisionworld.com/gcse-revision/ict/past-papers');
-              },
-              child: Image.asset(
-                'assets/revision.jpg',
-                width: 80, // Reduced width
-                height: 80, // Reduced height
-              ),
-            ),
-          ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButton(String imagePath, String url) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.15,
+      height: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          _launchURL(url);
+        },
+        child: Image.asset(imagePath),
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero,
+          ),
+          primary: Colors.brown,
         ),
       ),
     );
   }
 
-  _launchURL(String url) async {
+  void _updateVideo() {
+    if (_selectedExamBoard == 'AQA' && _selectedTopic == 'Programming Basics') {
+      _videoPlayerController = VideoPlayerController.asset('assets/alt_video.mp4');
+    } else {
+      _videoPlayerController = VideoPlayerController.asset('assets/example_video.mp4');
+    }
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController,
+      autoPlay: false,
+      looping: true,
+      aspectRatio: 16 / 9,
+      allowFullScreen: true,
+      showControlsOnInitialize: true,
+      showControls: true,
+      deviceOrientationsAfterFullScreen: [DeviceOrientation.portraitUp],
+      deviceOrientationsOnEnterFullScreen: [DeviceOrientation.landscapeRight, DeviceOrientation.landscapeLeft],
+    );
+  }
+
+  String _getLinkBitesize() {
+    if (_selectedExamBoard == 'AQA' && _selectedTopic == 'Programming Basics') {
+      return 'https://www.bbc.co.uk/bitesize/guides/zh66pbk/revision/1';
+    }
+    else {
+      return 'https://www.bbc.co.uk/bitesize/examspecs/zkwsjhv';
+    }
+  }
+
+  String _getLinkRevision() {
+    if (_selectedExamBoard == 'AQA' && _selectedTopic == 'Programming Basics') {
+      return 'https://revisionworld.com/gcse-revision/ict/past-papers/aqa-gcse-computer-science-past-papers';
+    } else {
+      return 'https://revisionworld.com/gcse-revision/ict/past-papers';
+    }
+  }
+
+  String _getLinkAda() {
+    if (_selectedExamBoard == 'AQA' && _selectedTopic == 'Programming Basics') {
+      return 'https://isaaccomputerscience.org/topics/gcse#aqa';
+    } else {
+      return 'https://isaaccomputerscience.org/?examBoard=all&stage=gcse';
+    }
+  }
+
+  void _launchURL(String url) async {
     if (await canLaunch(url)) {
       await launch(url);
     } else {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,6 +13,7 @@ import firebase_database
 import path_provider_foundation
 import share_plus
 import url_launcher_macos
+import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
@@ -23,4 +24,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
